18960ca811002238bf1ce39d8d56d9779266f396
mfumagalli <oceanodigomma@gmail.com>
2012-09-04 10:15:07 -0700

    Initial pasting of ngsCovar

---
+++ ngsCovar.cpp
@@ -0,0 +1,220 @@
+#include <cstdio>
+#include <cstdlib>
+#include <sys/stat.h>
+#include <cstring>
+#include <vector>
+#include <math.h>
+#include "ngsCoVar_supreme.hpp"
+
+// to compile: g++ -Wall -O0 -g ngsCoVar_supreme.cpp -o ngsCoVar_supreme
+
+// implement offset
+
+int main (int argc, char *argv[]) {
+  
+  // CALL HELP FUNCTION
+  if (argc==1) {
+    info();
+    return 0; // terminate
+  }
+ 
+  // DECLARE
+ 
+  // possible inputs
+  char *estfile=NULL; // estimated genotypes with probs (from angsd -doGeno 64)
+  char *sfsfile=NULL; // sfs probs (-realSFS 1 + optimSFS = sfstools [already normalized and not log]); this is a major change 
+  
+  FILE *outest;
+  char *outfiles=NULL;
+  char *foutest=NULL;
+  
+  int argPos = 1, increment = 0, nind = 0, nsites = 0, debug = 0, norm = 0, block_size = 10000, call=0, offset=0, maxgeno=0;
+  double esites = 0.0, minmaf = 0.0;
+ 
+  /// READ AND ASSIGN INPUT PARAMETERS
+  
+  while (argPos<argc) {
+    increment = 0; // leave this in case of future implementation of more parameters per argument
+    if(strcmp(argv[argPos],"-probfile")==0) 
+      estfile = argv[argPos+1];
+    else if(strcmp(argv[argPos],"-sfsfile")==0) 
+      sfsfile = argv[argPos+1];
+    else if(strcmp(argv[argPos],"-nind")==0) 
+      nind = atoi(argv[argPos+1]);
+    else if(strcmp(argv[argPos],"-nsites")==0) 
+      nsites = atoi(argv[argPos+1]);
+    else if(strcmp(argv[argPos],"-outfile")==0) 
+      outfiles = argv[argPos+1];
+    else if(strcmp(argv[argPos],"-norm")==0) 
+      norm = atoi(argv[argPos+1]);
+    else if(strcmp(argv[argPos],"-minmaf")==0) 
+      minmaf = atof(argv[argPos+1]);
+    else if(strcmp(argv[argPos],"-block_size")==0)
+      block_size = atoi(argv[argPos+1]);
+    else if(strcmp(argv[argPos],"-offset")==0)
+      offset = atoi(argv[argPos+1]);
+    else if(strcmp(argv[argPos],"-call")==0)
+      call = atoi(argv[argPos+1]);
+    else if(strcmp(argv[argPos],"-verbose")==0) 
+      debug = atoi(argv[argPos+1]);
+    else { // input is not a valid one 
+      printf("\tUnknown arguments: %s\n",argv[argPos]);
+      info();
+      return 0; // terminate
+    }
+    argPos = argPos + 2 + increment;
+  } // end while all inputs
+  
+  // check if there is the input file
+  if ((estfile == NULL)) {
+    fprintf(stderr,"\nMust supply -probfile.\n");
+    info();
+    return 0;
+  }
+
+  // check if there is the sfs file
+  if ((sfsfile != NULL) & (minmaf>0)) {
+    fprintf(stderr,"\n-sfsfile and -minmaf are in conflict! If use use -sfsfile to weight each file there is no need (theoretically) to filter out sites with low maf. In this case -minmaf won't be applied and the program terminates here.\n");
+    info();
+    return 0;
+  }
+  
+  // check if there is the output file
+  if(outfiles == NULL) {
+    fprintf(stderr,"\nMust supply -outfile.\n");
+    info();
+    return 0;
+  }
+
+  // if block_size longer than nsites
+  if (block_size>nsites) block_size=nsites;
+
+  // prepare output files
+  foutest = append(outfiles, "");
+  
+  // print input arguments
+  fprintf(stderr,"\t->Using args: -nind %d -nsites %d -probfile %s -sfsfile %s -outfile %s -verbose %d -norm %d -minmaf %f -block_size %d -call %d -offset %d\n", nind, nsites, estfile, sfsfile, foutest, debug, norm, minmaf, block_size, call, offset); 
+ 
+  // BLOCKS
+  int nwin = (nsites/block_size);
+  /// GET POSITIONS OF BLOCKS
+  array<int> start; array<int> end;
+  start=getStart(nsites, 0, block_size);
+  end=getEnd(nsites, 0, block_size);
+  int maxlen=end.data[0]-start.data[0]+1; // len for each win, it will never be greater than this
+ 
+  // prepare out
+  fprintf(stderr,"\t->Dumping file: %s\n", foutest);
+  outest = getFILE(foutest, "w");
+
+  // initialize covariance matrix  
+  matrix<double> covar;
+  double **cdata = new double*[nind];
+  for(int i=0;i<nind;i++){
+    double *ctmp = new double[nind];
+    cdata[i]= ctmp;
+  }
+  covar.x = nind;
+  covar.y = nind;
+  covar.data = cdata;
+  for (int i=0;i<nind;i++) {
+    for (int j=0;j<nind;j++) {
+      covar.data[i][j]=0.0;
+    }
+  }
+
+  // initialize pvar
+  array<double> pvar;
+  double *temp2 = new double [maxlen]; // init to the size of the largest window
+  for (int i=0; i<maxlen; i++)
+   temp2[i]=0.0;
+  pvar.x=maxlen;
+  pvar.data=temp2;
+
+  // ITERATING for each block
+  for (int n=0; n<nwin; n++) {
+
+    matrix<double> sfs;
+    matrix<double> esti;
+    array<double> pp;
+
+    fprintf(stderr, "Block %d out of %d from %d to %d\n", n, (nwin-1), start.data[n], end.data[n]);
+
+    // compute esti
+    if (debug==1) fprintf(stderr, "\nGetting esti...");
+    esti = readEstiSub(estfile, nind, nsites, start.data[n], end.data[n]);
+    if (debug==2) writematrix(esti, stderr);
+    if (debug==1) fprintf(stderr, ": %d %d , %f %f", esti.x, esti.y, esti.data[0][0], esti.data[1][1]);  
+
+    // IF CALL GENOTYPES, set max prob to 1
+    if (call) {
+      if (debug==1) fprintf(stderr, "\nCalling genotypes...");
+      for (int i=0; i<esti.x; i++) {
+        maxgeno=maxposarr(esti, i);
+        for (int j=0; j<3; j++) esti.data[i][j]=0.0;
+        esti.data[i][maxgeno]=1.0;
+      }
+    }
+
+    /// COMPUTE ALLELE FREQUENCIES
+    if (debug==1) fprintf(stderr, "\nGetting mu...");
+    pp = getAlleFreq(esti);
+    if (debug==3) writearray(pp, stderr);
+    // this is NOT strictly the alle freq, is mu in Patterson notation, thus it's the average number of derived alleles per individual; the allele freq p is simply mu/2
+    if (debug==2) {
+      for (int i=0; i<pp.x; i++)
+       if (pp.data[i]<0) fprintf(stderr, "\n %d %f", i, pp.data[i]);
+    }
+    if (debug==1) fprintf(stderr, ": %d, %f %f ", pp.x, pp.data[0], pp.data[4]);
+ 
+    /// COMPUTE COVARIANCE, here simply update
+    if (debug==1) fprintf(stderr, "\nUpdating covar...");
+    if (sfsfile!=NULL) {
+      // read sfs
+      fprintf(stderr, "...weighting...");
+      sfs = readFileSub(sfsfile, nind, start.data[n], end.data[n], 0);
+      if (debug==1) fprintf(stderr, "\nGot  sfs: %d %d, e.g. %f %f", sfs.x, sfs.y, sfs.data[0][0], sfs.data[1][1]);
+      if (debug==1) fprintf(stderr, "\nGetting pvar...");
+      getPvar(sfs, pvar);
+      if (debug==1) fprintf(stderr, ": %d , %f %f", pvar.x, pvar.data[0], pvar.data[10]);
+      cleanup(sfs);
+      if (debug==1) fprintf(stderr, "\nUpdating covar...");
+      calcCovarUpProb(esti, pp, norm, covar, pvar);
+      if (debug==1) fprintf(stderr, "\nUpdating esite...");
+      // divide by expected nr of segr sites -1 in case of pvar
+      for (int i=0;i<pvar.x;i++) esites = esites + pvar.data[i];
+      if (debug==1) fprintf(stderr, ": %f", esites);
+    } else {
+      if (debug==1) fprintf(stderr, "\n I am using this minmaf %f ", minmaf);
+      fprintf(stderr, "...no weighting..."); 
+      double tmp_eff_nsites=calcCovarUp(esti, pp, norm, covar, minmaf);
+      esites=esites+tmp_eff_nsites;
+    }
+       
+    cleanup(esti);
+
+    delete [] pp.data;
+  
+  } // end for n in nwin block
+
+  fprintf(stderr, "\n(Exp/eff) nr sites: %f\n", esites);
+
+  delete [] pvar.data;
+
+  // divide
+  for (int i=0;i<nind;i++) {
+    for (int j=0;j<nind;j++) {
+      covar.data[i][j]=covar.data[i][j]/(esites-1);
+    }
+  }
+
+  writematrix(covar, outest);  
+  
+  // free
+  cleanup(covar);
+  free(foutest);
+    
+  return 0;
+  
+} // end main
+
---
+++ ngsCovar.hpp
@@ -0,0 +1,384 @@
+/// TEMPLATES
+
+#include <string> //for str operations
+using namespace std;
+
+// a general matrix style structure
+template <typename T>
+struct matrix{
+  int x;
+  int y;
+  T** data;
+};
+
+template <typename T>
+struct array{
+  int x;
+  T* data;
+};
+
+template <typename T>
+T *collapse(std::vector<T> &v){
+  T *tmp = new T[v.size()];
+  for(int i=0;i<v.size();i++)
+    tmp[i] = v[i];
+  return tmp;
+}
+
+//function to cleanup our generic matrix structure
+template <typename T>
+void cleanup(matrix<T> &m){//using a reference to avoid copying the data
+  for(int i=0;i<m.x;i++)
+    delete [] m.data[i];
+  delete [] m.data;
+}
+
+/// FUNCTIONS
+
+array<int> getStart(int nsites, int firstbase, int block_size) {
+  int nwin = ((nsites-firstbase)/block_size);
+  array<int> start;
+  if ( ((nsites-firstbase) % block_size)!=0) {
+    start.x=nwin+1;
+  } else {
+    start.x=nwin;
+  }
+  int *tStart= new int [nwin];
+  for (int i=0; i<nwin; i++) {
+    tStart[i]=(i)*block_size;
+  }
+  // if there is rest
+  if ( (nsites % block_size)!=0) {
+    tStart[nwin]=tStart[(nwin-1)]+block_size;
+    nwin=nwin+1;
+  }
+  // if you dont start from beginning
+  if (firstbase>0) {
+    for (int i=0; i<nwin; i++) {
+      tStart[i]=tStart[i]+firstbase;
+    }
+  }
+  start.data=tStart;
+  return start;
+}
+
+array<int> getEnd(int nsites, int firstbase, int block_size) {
+  int nwin = ((nsites-firstbase)/block_size);
+  array<int> end;
+  if ( ((nsites-firstbase) % block_size)!=0) {
+    end.x=nwin+1;
+  } else {
+    end.x=nwin;
+  }
+  int *tEnd= new int [nwin];
+  for (int i=0; i<nwin; i++) {
+    tEnd[i]=(i+1)*block_size-1;
+  }
+  // if there is rest
+  if ( (nsites % block_size)!=0) {
+    tEnd[nwin]=nsites-1;
+    nwin=nwin+1;
+  }
+  // if you dont start from beginning
+  if (firstbase>0) {
+    for (int i=0; i<nwin; i++) {
+      tEnd[i]=tEnd[i]+firstbase;
+    }
+  }
+  end.data=tEnd;
+  return end;
+}
+
+
+// get the filesize of afile
+size_t fsize(const char* fname){
+  struct stat st ;
+  stat(fname,&st);
+  return st.st_size;
+}
+
+// find out if a file exists
+int fexists(const char* str) {
+  struct stat buffer ;
+  return (stat(str, &buffer )==0 );
+}
+
+// a nice wrapper for getting files
+FILE *getFILE(const char*fname,const char* mode) {
+  int writeFile = 0;
+  for(size_t i=0;i<strlen(mode);i++)
+    if(mode[i]=='w')
+      writeFile = 1;
+  if(writeFile&&fexists(fname)){
+    fprintf(stderr,"\t-> File exists: %s exiting...\n",fname);
+    exit(0);
+  }
+  FILE *fp;
+  if(NULL==(fp=fopen(fname,mode))){
+    fprintf(stderr,"\t->Error opening FILE handle for file:%s exiting\n",fname);
+    exit(0);
+  }
+  return fp;
+}
+
+// read a file into a matrix but only for a specific subsets of positions (0-based notation)
+matrix<double> readFileSub(char *fname, int nInd, int start, int end, int isfold) {
+  FILE *fp = getFILE(fname,"r");
+  size_t filesize =fsize(fname);
+  if (isfold==0) {
+    if((filesize %(sizeof(double)*(2*nInd+1)) )) {
+      fprintf(stderr,"\n\t-> Possible error,binaryfiles might be broken\n");
+      exit(-1);
+    }
+  } else {
+    if((filesize %(sizeof(double)*(nInd+1)) )) {
+      fprintf(stderr,"\n\t-> Possible error,binaryfiles might be broken\n");
+      exit(-1);
+    }	  
+  }
+  int nsites = end-start+1;  
+  double **data = new double*[nsites];
+  if (isfold) {
+	  fseek(fp, sizeof(double)*(nInd+1)*start, SEEK_SET);
+  } else {
+	  fseek(fp, sizeof(double)*(2*nInd+1)*start, SEEK_SET);
+  }
+  if (isfold) {
+    for(int i=0; i<nsites; i++) {
+      double *tmp = new double[nInd+1];
+      fread(tmp,sizeof(double),nInd+1,fp);
+      data[i]= tmp;
+    }
+  } else {
+    for(int i=0; i<nsites; i++) {
+      double *tmp = new double[2*nInd+1];
+      fread(tmp,sizeof(double),2*nInd+1,fp);
+      data[i]= tmp;
+    }
+  }
+  fclose(fp);
+  matrix<double> ret;
+  ret.x = nsites;
+  if (isfold) {
+    ret.y = nInd+1;
+  } else {
+    ret.y = 2*nInd+1;
+  }
+  ret.data = data;
+  return ret;
+}
+
+// read genotype posterior probabilities from angsd (-dogeno 64), but only for a specific subsets of positions (0-based notation)
+matrix<double> readEstiSub(char *fname, int nInd, int nSites, int start, int end) {
+  FILE *fp = getFILE(fname,"rb");
+  size_t filesize =fsize(fname);
+  if((filesize %(sizeof(double)*(3*nInd*nSites)) )) {
+    fprintf(stderr,"\n\t-> Possible error,binaryfiles might be broken\n");
+    exit(-1);
+  }
+  int nsites = end-start+1;
+  double **data = new double*[nsites];
+  fseek(fp, sizeof(double)*(3*nInd)*start, SEEK_SET);
+  for(int i=0; i<nsites; i++) {
+    double *tmp = new double[3*nInd];
+    fread(tmp,sizeof(double),3*nInd,fp);
+    data[i]= tmp;
+  }
+  fclose(fp);
+  matrix<double> ret;
+  ret.x = nsites;
+  ret.y = 3*nInd;
+  ret.data = data;
+  return ret;
+}
+
+// return max value position of a row from a matrix
+int maxposarr(matrix<double> &m, int row) {
+  int i=0, res = 0;
+  double val;
+  val = m.data[row][0];
+  for (i = 1; i < m.y; i++) {
+    if (m.data[row][i] > val) {
+      res = i;
+      val = m.data[row][i];
+    }
+  }
+  return res;
+}
+
+// get probability of being variable from posterior probabilities of sample allele frequencies
+void getPvar(matrix<double> &sfs, array<double> pvar) {
+  for (int i=0; i<sfs.x; i++)
+    pvar.data[i]=1-sfs.data[i][0]-sfs.data[i][2*(sfs.y-1)];
+}
+
+// write an array into a file
+void writearray(array<double> &m,FILE *fp){
+  for(int i=0;i<m.x;i++)
+    fprintf(fp,"%f\t",m.data[i]);
+  fprintf(fp,"\n");
+  
+}
+
+// write an array of Int into a file
+void writearrayInt(array<int> &m,FILE *fp){
+  for(int i=0;i<m.x;i++)
+    fprintf(fp,"%d\t",m.data[i]);
+  fprintf(fp,"\n");
+}
+
+// write a matrix into a file
+void writematrix(matrix<double> &m,FILE *fp){
+  for(int i=0;i<m.x;i++){
+    for(int j=0;j<m.y;j++)
+      fprintf(fp,"%f\t",m.data[i][j]);
+    fprintf(fp,"\n");
+  }
+}
+
+// write a matrix of Int into a file
+void writematrixInt(matrix<int> &m,FILE *fp){
+  for(int i=0;i<m.x;i++){
+    for(int j=0;j<m.y;j++)
+      fprintf(fp,"%d\t",m.data[i][j]);
+    fprintf(fp,"\n");
+  }
+}
+
+// to append names
+char *append(const char* a,const char *b){
+  char *c =(char *) malloc((strlen(a)+strlen(b)+1)*sizeof(char));
+  strcpy(c,a);
+  strncat(c,b,strlen(b));
+  return c;
+}
+
+
+// print help
+void info() {
+  fprintf(stdout, "\ninput:\n-probfile: file with genotype posterior probabilities [required], this is the output from angsd -doGeno 64\n-outfile: name of output file [required], currently it is a text file, tab separated with n*n cells\n-sfsfile: file with SFS posterior probabilities [required if you want to weight each site by its probability of being variable], this is the binary output of sfstools after running realSFS1 and optimSFS\n-nind: nr of individuals [required];\n-nsites: nr of sites [required]\nnorm: which method to use to normalize the covariance matrix [optional] as in Patterson et al (0), using classic standard deviation (1), or no normalization (>1); default and suggested value is 0\n-verbose: level of verbosity [optional]\n-block_size: how many sites per block when reading the input file [optiona], default is 1 block equal to nr of sites, suggested 10K-20K\n-call: whether calling genotypes (1) or not (0), default\n-offset: starting position of subset analysis\n-minmaf: filter out sites with estimated MAF less than minmaf or greater than 1-minmaf [optional], default is 0; please note that this filtering won't take action when using the weighting approach because in teory you won't need that in that case.\n\nExample:\nngsSim -outfiles pops -npop 2 -nind 10 10 -nsites 1000 -pvar 1 -F 0.3 0.3\nangsd.g++ -sim1 pops.glf.gz -nInd 20 -doGeno 64 -doMajorMinor 1 -doMaf 2 -outfiles pops.geno\nangsd.g++ -sim1 pops.glf.gz -nInd 20 -realSFS 1 -outfiles POPS\noptimSFS.gcc -binput POPS.sfs -nChr 40 -nThread 10\nsfstools.g++ -sfsFile POPS.sfs -nChr 40 -priorFile POPS.sfs.ml -dumpBinary 1 > POPS.norm.sfs\n# no weighting: ngsCoVar_ultimate_fast -probfile pops.geno.geno -outfile no -nind 20 -nsites 1000 -verbose 0 -block_size 500 -norm 0\n# weighting: ngsCoVar_ultimate_fast -probfile pops.geno.geno -outfile si -nind 20 -nsites 1000 -verbose 0 -block_size 500 -norm 0 -sfsfile POPS.norm.sfs\n# no weighting but cutoff on maf: ngsCoVar_ultimate_fast -probfile pops.geno.geno -outfile me -nind 20 -nsites 1000 -verbose 0 -block_size 500 -norm 0 -minmaf 0.1\n\nPlease note that this has been tested only with angsd 0.204\n");
+}
+
+// compute estimated allele frequencies from genotype posterior probabilities
+array<double> getAlleFreq (matrix<double> &m) {
+  // m is dimensions: nsites * (nind*3)
+  int nsites = m.x;
+  int nind = m.y/3;
+  double somma;
+  double *tmp = new double[nsites];
+  for (int i=0; i<nsites; i++) {
+    somma = 0.0;
+    for (int j=0; j<nind; j++) {
+      somma = somma + m.data[i][(j*3)+1] + (2*m.data[i][(j*3)+2]);
+    }
+    tmp[i] = somma / (nind*2);    
+  }  
+  array<double> ret;
+  ret.x = nsites;
+  ret.data = tmp;
+  return ret;
+}
+
+// get the covariance for a pair of individual; output is the effective number of sites (expected or passed the filter)
+double calcCovarUp (matrix<double> &m, array<double> a, int norm, matrix<double> &covar, double minmaf) {
+  int nsites = m.x;
+  int nind = m.y/3;
+  if (nsites != a.x) {
+    fprintf(stderr, "\n prob file and alle freq dimensions disagree. Terminate.");
+    exit(-1);
+  }
+  double eff_nsites=0.0;
+  for (int s=0; s<nsites; s++) {
+    if ((a.data[s]>minmaf) & (a.data[s]<(1-minmaf))) {
+      eff_nsites=eff_nsites+1.0;
+    }
+  }
+  double somma = 0.0, subsomma = 0.0;
+  double **data = new double*[nind];
+  for (int i=0; i<nind; i++) {
+    double *tmp = new double[nind];
+    for (int j=0; j<(i+1); j++) {
+      somma = 0.0;
+      for (int s=0; s<nsites; s++) {
+	subsomma = 0.0;
+        if ((a.data[s]>minmaf) && (a.data[s]<(1-minmaf))) {
+	  for (int C1=0; C1<3; C1++) {
+            for (int C2=0; C2<3; C2++) {
+             subsomma = subsomma + (C1-(2*a.data[s]))*(C2-(2*a.data[s]))*m.data[s][(i*3)+C1]*m.data[s][(j*3)+C2];
+            }
+	  }
+          if (norm==0) {
+	    subsomma = subsomma / (2*a.data[s]*(1-a.data[s])); 
+	  } else if (norm==1) {
+   	    subsomma = subsomma / sqrt(2*a.data[s]*(1-a.data[s])); 
+	  } 
+        }
+	somma = somma + subsomma;
+      }
+      tmp[j] = somma;
+    }
+    data[i] = tmp;
+  }
+  fprintf(stderr, "\n nsites is %d but effective is %f", nsites, eff_nsites);
+  matrix<double> ret;
+  ret.x = nind;
+  ret.y = nind;
+  ret.data = data;
+  for (int i=0;i<covar.x;i++) {
+    for (int j=0;j<(i+1);j++) {
+      covar.data[i][j]=covar.data[i][j]+ret.data[i][j];
+      covar.data[j][i]=covar.data[j][i]+ret.data[i][j];
+    }
+  }
+  cleanup(ret);
+  return eff_nsites;  
+}
+
+
+// get the covariance for a pair of individual by weighting by
+void calcCovarUpProb (matrix<double> &m, array<double> a, int norm, matrix<double> &covar, array<double> pvar) {
+  int nsites = m.x;
+  int nind = m.y/3;
+  if (nsites != a.x) {
+    fprintf(stderr, "\n prob file and alle freq dimensions disagree. Terminate.");
+    exit(-1);
+  }
+  double somma = 0.0, subsomma = 0.0;
+  double **data = new double*[nind];
+  for (int i=0; i<nind; i++) {
+    double *tmp = new double[nind];
+    for (int j=0; j<(i+1); j++) {
+      somma = 0.0;
+      for (int s=0; s<nsites; s++) {
+	  subsomma = 0.0;
+	  for (int C1=0; C1<3; C1++) {
+            for (int C2=0; C2<3; C2++) {
+	      subsomma = subsomma + (C1-(2*a.data[s]))*(C2-(2*a.data[s]))*m.data[s][(i*3)+C1]*m.data[s][(j*3)+C2];
+            }
+	  }
+          subsomma=subsomma*pvar.data[s];
+          if (norm==0) {
+	    subsomma = subsomma / (2*a.data[s]*(1-a.data[s])); 
+	  } else if (norm==1) {
+   	    subsomma = subsomma / sqrt(2*a.data[s]*(1-a.data[s])); 
+	  } 
+	somma = somma + subsomma;
+      }
+      tmp[j] = somma;
+    }
+    data[i] = tmp;
+  }
+  matrix<double> ret;
+  ret.x = nind;
+  ret.y = nind;
+  ret.data = data;
+  for (int i=0;i<covar.x;i++) {
+    for (int j=0;j<(i+1);j++) {
+      covar.data[i][j]=covar.data[i][j]+ret.data[i][j];
+      covar.data[j][i]=covar.data[j][i]+ret.data[i][j];
+    }
+  }
+  cleanup(ret);
+}
+
+
