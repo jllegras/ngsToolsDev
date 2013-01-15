8f7a2d3c4185db9669575f1f52428b83969a556e
mfumagalli <oceanodigomma@gmail.com>
2013-01-15 11:26:21 -0800

    Updated getMultiFST.R for multiple subwindows

--- getMultiFST.R
+++ getMultiFST.R
@@ -1,16 +1,36 @@
-getMultiFST<-function(filein, fileout, from_known=FALSE) {
+getMultiFST<-function(filein, fileout, from_known=FALSE, len_win=0) {
 
-  # this script converts the output of ngsFST and compute multiple-site FST and rewrite the file with this new values of FST (to be used as -firstfile) 
+	# this script converts the output of ngsFST and compute multiple-site FST and rewrite the file with this new values of FST (to be used as -firstfile) 
 
-  val=c()
-  if (from_known) { # this is for compatibility with gimmeFST
-    rt=read.table(filein,head=T)
-    val=sum(rt[,8])/sum(rt[,9])
-  } else {
-    rt=read.table(filein,head=F)
-    val=sum(rt[,1])/sum(rt[,2])
-  }
+	# len_win: length of windows with same FST. In practice it is usually to assign the same FST to multiple site for each window (a subset of your data) by computing the local FST
 
-  write.table(cbind(rep(0,nrow(rt)),rep(0,nrow(rt)),rep(0,nrow(rt)),rep(val,nrow(rt)),rep(0,nrow(rt))), sep="\t", quote=F, row.names=F, col.names=F, file=fileout)
+
+	val=c()
+	if (from_known) { # this is for compatibility with gimmeFST
+
+		rt=read.table(filein,head=T)
+		ltot=nrow(ltot);
+                if (len_win>0) {
+                        start=seq(1, ltot, len_win);
+                        end=c(seq(len_win+1, ltot, len_win), ltot); end=end[1:length(start)];
+                        for (f in 1:length(start)) val=c(val, sum(rt[start[f]:end[f],8])/sum(rt[start[f]:end[f],9]))
+                } else {
+                        val=sum(rt[,8])/sum(rt[,9])
+                }
+
+  	} else {
+
+    		rt=read.table(filein,head=F)
+    		ltot=nrow(ltot);
+    		if (len_win>0) {
+     			start=seq(1, ltot, len_win);
+     			end=c(seq(len_win+1, ltot, len_win), ltot); end=end[1:length(start)];
+     			for (f in 1:length(start)) val=c(val, sum(rt[start[f]:end[f],1])/sum(rt[start[f]:end[f],2]))
+    		} else {
+      			val=sum(rt[,1])/sum(rt[,2])
+    		}
+  	}
+
+	write.table(cbind(rep(0,nrow(rt)),rep(0,nrow(rt)),rep(0,nrow(rt)),rep(val,nrow(rt)),rep(0,nrow(rt))), sep="\t", quote=F, row.names=F, col.names=F, file=fileout)
 
 }
