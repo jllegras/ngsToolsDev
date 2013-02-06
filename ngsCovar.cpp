7c2fe909f53c03ddac4b78dcab16731a03b1151c
mfumagalli <oceanodigomma@gmail.com>
2013-02-05 18:07:58 -0800

    Debug nsgCovar

--- ngsCovar.cpp
+++ ngsCovar.cpp
@@ -236,7 +236,7 @@ int main (int argc, char *argv[]) {
       }
       fprintf(stderr, "\t Sum:%f", temp_sum);
 
-      if (temp_sum<0) {
+      if (temp_sum==-nan) {
         for (int i=0; i<pvar.x; i++) fprintf(stderr, "\n %d %f", i, temp_sum);
         exit(0);
       }
