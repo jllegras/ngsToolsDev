2df6d639524c093d4074b3ec0715ce33a61ef202
fgvieira <fgarrettvieira@gmail.com>
2013-08-21 10:41:18 -0700

    Changed to use ggplot2 library

--- plotPCA.R
+++ plotPCA.R
@@ -1,6 +1,8 @@
 
 # Usage: Rscript infile.covar component1 component2 outfile.eps npop nind1 nind2 ... nindNPOP
 
+library(ggplot2)
+
 # Read parameters
 args <- commandArgs(trailingOnly = TRUE);
 infile <- args[1];
@@ -10,7 +12,6 @@ outfile <- args[4];
 npop <- as.integer(args[5]);
 nind <- as.array(args[6:(npop+5)]);
 rm(args);
-cols <- c(); for (i in 1:npop) cols <- c(cols, rep(rainbow(npop)[i], nind[i]));
 
 # Read input file
 covar <- read.table(infile, stringsAsFact=F);
@@ -21,9 +22,13 @@ eig$val <- eig$val/sum(eig$val);
 cat(signif(eig$val, digits=3)*100,"\n");
 
 # Plot
-x11();
-plot(eig$vec[,comp1], eig$vec[,comp2], col=cols, xlab=paste("PC",comp1," (",signif(eig$val[comp1], digits=3)*100,"%)",sep="",collapse=""), ylab=paste("PC",comp2," (",signif(eig$val[comp2], digits=3)*100,"%)",sep="",collapse=""), main="PCA", sub=""); 
-dev.copy2eps(file=outfile);
-dev.off();
+PC <- as.data.frame(eig$vectors)
+colnames(PC) <- gsub("V", "PC", colnames(PC))
+id <- c(); for (i in 1:npop) id <- c(id, rep(paste("Pop",i), nind[i]));
+PC$Pop <- id
 
+title <- paste("PC",comp1," (",signif(eig$val[comp1], digits=3)*100,"%)"," / PC",comp2," (",signif(eig$val[comp2], digits=3)*100,"%)",sep="",collapse="")
 
+ggplot() + ggtitle(title) + geom_point(data=PC, aes_string(x=paste("PC",comp1,sep=""), y=paste("PC",comp2,sep=""), color="Pop"))
+ggsave(outfile)
+unlink("Rplots.pdf", force=TRUE)
