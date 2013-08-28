44026a1705c7af83d90a4a491f6aae48bc2a44c1
fgvieira <fgarrettvieira@gmail.com>
2013-08-28 15:32:44 -0700

    Fixed minor bugs

--- plotPCA.R
+++ plotPCA.R
@@ -1,34 +1,42 @@
 
 # Usage: Rscript infile.covar component1 component2 outfile.eps npop nind1 nind2 ... nindNPOP
-
+library(optparse)
 library(ggplot2)
 
-# Read parameters
-args <- commandArgs(trailingOnly = TRUE);
-infile <- args[1];
-comp1 <- as.integer(args[2]);
-comp2 <- as.integer(args[3]);
-outfile <- args[4];
-npop <- as.integer(args[5]);
-nind <- as.array(args[6:(npop+5)]);
-rm(args);
+option_list <- list(make_option(c('-i','--in_file'), action='store', type='character', default=NULL, help='Input file'),
+                    make_option(c('-c','--comp'), action='store', type='character', default=1-2, help='Components to plot'),
+                    make_option(c('-a','--annot_file'), action='store', type='character', default=NULL, help='Annotation file with individual populations'),
+                    make_option(c('-o','--out_file'), action='store', type='character', default=NULL, help='Output file')
+                    )
+opt <- parse_args(OptionParser(option_list = option_list))
+
+#################################################################################
 
 # Read input file
-covar <- read.table(infile, stringsAsFact=F);
-eig <- eigen(covar, symm=TRUE);
+covar <- read.table(opt$in_file, stringsAsFact=F);
+
+# Read annot file
+annot <- read.table(opt$annot_file, sep="\t");
+colnames(annot)[6] <- "Pop"
+
+# Parse components to analyze
+comp <- as.numeric(strsplit(opt$comp, "-", fixed=TRUE)[[1]])
 
 # Eigenvalues
+eig <- eigen(covar, symm=TRUE);
 eig$val <- eig$val/sum(eig$val);
 cat(signif(eig$val, digits=3)*100,"\n");
 
 # Plot
 PC <- as.data.frame(eig$vectors)
 colnames(PC) <- gsub("V", "PC", colnames(PC))
-id <- c(); for (i in 1:npop) id <- c(id, rep(paste("Pop",i), nind[i]));
-PC$Pop <- id
+PC$Pop <- factor(annot$Pop)
 
-title <- paste("PC",comp1," (",signif(eig$val[comp1], digits=3)*100,"%)"," / PC",comp2," (",signif(eig$val[comp2], digits=3)*100,"%)",sep="",collapse="")
+title <- paste("PC",comp[1]," (",signif(eig$val[comp[1]], digits=3)*100,"%)"," / PC",comp[2]," (",signif(eig$val[comp[2]], digits=3)*100,"%)",sep="",collapse="")
 
-ggplot() + ggtitle(title) + geom_point(data=PC, aes_string(x=paste("PC",comp1,sep=""), y=paste("PC",comp2,sep=""), color="Pop"))
-ggsave(outfile)
+x_axis = paste("PC",comp[1],sep="")
+y_axis = paste("PC",comp[2],sep="")
+
+ggplot() + geom_point(data=PC, aes_string(x=x_axis, y=y_axis, color="Pop")) + ggtitle(title)
+ggsave(opt$out_file)
 unlink("Rplots.pdf", force=TRUE)
